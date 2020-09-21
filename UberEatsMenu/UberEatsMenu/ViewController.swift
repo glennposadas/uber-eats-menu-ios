41e6fa7e70c2bdfe1e4e7df007eb2fdc07e09021
Glenn Posadas <hello@glennvon.com>
2020-09-22 01:23:41 +0800

    add fresh project

---
+++ UberEatsMenu/UberEatsMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberEatsMenu
+//
+//  Created by Glenn Von Posadas on 9/22/20.
+//  Copyright © 2020 Glenn Posadas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
